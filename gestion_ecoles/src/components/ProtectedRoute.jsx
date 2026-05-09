257867b586e068bdd3b17cf1bb4f2e2aa800a2c8
vivi2705-ui <jeenoria691@gmail.com>
2026-05-09 16:31:52 +0000

    Update ProtectedRoute.jsx

--- gestion_ecoles/src/components/ProtectedRoute.jsx
+++ gestion_ecoles/src/components/ProtectedRoute.jsx
@@ -8,20 +8,4 @@ export default function ProtectedRoute() {
 
   return user ? <Outlet /> : <Navigate to="/login" replace />;
 }
-// // src/components/ProtectedRoute.jsx
-// import { Navigate, Outlet } from 'react-router-dom';
-// import { useAuth } from '../context/AuthContext';
 
-// export default function ProtectedRoute() {
-//   const { user, loading } = useAuth();
-
-//   if (loading) {
-//     return (
-//       <div className="d-flex justify-content-center align-items-center min-vh-100">
-//         <div className="spinner-border text-primary" role="status" />
-//       </div>
-//     );
-//   }
-
-//   return user ? <Outlet /> : <Navigate to="/login" replace />;
-// }
