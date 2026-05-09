c4aff01e9f3a2a4c6574ff3523348cbafda635ce
vivi2705-ui <jeenoria691@gmail.com>
2026-05-09 16:28:27 +0000

    Update verifyToken.js

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -18,17 +18,3 @@ module.exports = (req, res, next) => {
     return res.sendStatus(401);
   }
 };
-// const jwt = require('jsonwebtoken');
-// const SECRET = process.env.JWT_SECRET;
-
-// module.exports = (req, res, next) => {
-//   const token = req.headers.authorization?.split(' ')[1];
-//   if (!token) return res.sendStatus(401);
-
-//   try {
-//     req.utilisateur = jwt.verify(token, SECRET);
-//     next();
-//   } catch {
-//     res.sendStatus(401);
-//   }
-// };
